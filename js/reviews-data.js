db9d01161434496a08b62d0fd0976f6b01043cf9
Pedrolacarcel98 <pedrolacarceljardi@gmail.com>
2026-06-04 20:06:38 +0200

    Nuevo carrusel de reviews + nueva imagen para inicio + desplegable en index de servicios

---
+++ js/reviews-data.js
@@ -0,0 +1,48 @@
+/**
+ * Datos de las reseñas de Google.
+ * Actualizado manualmente con reseñas reales el 04/06/2026.
+ */
+window.googleReviews = [
+  {
+    "author_name": "Pedro Lacárcel Jardi",
+    "rating": 5,
+    "text": "Mi padre ha quedado muy satisfecho con la primera sesión. La semana que viene continuaremos con la rehabilitación, muchas gracias al equipo por el servicio. Un saludo.",
+    "relative_time_description": "Hace 2 meses",
+    "profile_photo_url": ""
+  },
+  {
+    "author_name": "Angel María Martín Galán",
+    "rating": 5,
+    "text": "Excelente trato. Muy profesional y empático. Ejercicios progresivos, adaptados a las necesidades y orientados a una recuperación progresiva, sin someter al paciente a esfuerzos que puedan generar rechazo o refractariedad al tratamiento. Además, se adecua a los cambios de agenda. Se van viendo resultados, aun con poco tiempo de tratamiento. Ni un reproche. Gracias.",
+    "relative_time_description": "Hace 4 meses",
+    "profile_photo_url": ""
+  },
+  {
+    "author_name": "Marta Morote",
+    "rating": 5,
+    "text": "Muy buen trato y muy buenos profesionales. Se desplazan a domicilio con todo el material necesario. En mi caso, los avisé con muy poco tiempo de antelación por una urgencia, y aun así fueron súper puntuales. Sin ninguna duda los volvería a elegir.",
+    "relative_time_description": "Hace 8 meses",
+    "profile_photo_url": ""
+  },
+  {
+    "author_name": "Mª Angeles Delgado García",
+    "rating": 5,
+    "text": "El servicio ha sido impecable. Me ha ayudado mucho, ya que me encontraba bastante mal, y se nota que realmente se preocupa por sus pacientes. El trato es muy cercano y profesional. Lo recomiendo totalmente!!",
+    "relative_time_description": "Hace 8 meses",
+    "profile_photo_url": ""
+  },
+  {
+    "author_name": "MPV NL",
+    "rating": 5,
+    "text": "Un gran servicio, al igual que muy profesional. Contacté con Jero ya que un familiar mayor tenía muchas molestias en el cuello desde hace tiempo. Mi abuela quedó encantada con el trato y la involucración recibidas durante la sesión. Además, le dio algunos consejos y una serie de ejercicios para que hiciera por su cuenta. Una gran ventaja el desplazamiento a domicilio. Muy recomendable. ¡Repetiremos seguro!",
+    "relative_time_description": "Hace 10 meses",
+    "profile_photo_url": ""
+  },
+  {
+    "author_name": "Paula Roales",
+    "rating": 5,
+    "text": "Un equipo excepcional. Trato cercano, profesionalidad absoluta y resultados desde la primera sesión. Totalmente recomendados.",
+    "relative_time_description": "Hace 6 meses",
+    "profile_photo_url": ""
+  }
+];
